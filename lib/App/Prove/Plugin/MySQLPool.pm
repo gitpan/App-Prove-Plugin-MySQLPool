8ac037e2ca2dfe81d25cda9df7ca647292b8df71
Masakazu Ohtsuka <mash@cpan.org>
2013-03-14 14:43:05 +0000

    Import of MASH/App-Prove-Plugin-MySQLPool-0.03 from CPAN.
    gitpan-cpan-distribution: App-Prove-Plugin-MySQLPool
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MASH/App-Prove-Plugin-MySQLPool-0.03.tar.gz
    gitpan-cpan-author:       MASH
    gitpan-cpan-maturity:     released

--- lib/App/Prove/Plugin/MySQLPool.pm
+++ lib/App/Prove/Plugin/MySQLPool.pm
@@ -5,7 +5,7 @@ use File::Temp;
 use POSIX::AtFork;
 use Test::mysqld::Pool;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub load {
     my ($class, $prove) = @_;
@@ -25,8 +25,8 @@ sub load {
             push( @INC, 'lib' )
                 if $lib;
 
-            eval "require $preparer"
-                or die "$@"; ## no critic
+            eval "require $preparer" ## no critic
+                or die "$@";
 
             $preparer->prepare( $mysqld );
         } ) : ()),
